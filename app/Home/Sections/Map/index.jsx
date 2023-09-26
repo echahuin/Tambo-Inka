14ae815db93e59cc5ea16a888e4f4ec3085d7af0
Edgar <edgachahuinauccafuro@frba.utn.edu.ar>
2023-09-26 19:23:07 -0300

    add paddings

--- app/Home/Sections/Map/index.jsx
+++ app/Home/Sections/Map/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function index() {
   return (
-    <div style={{display: "flex", alignItems: "center", justifyContent: "center", padding: "1rem"}} >       
+    <div style={{display: "flex", alignItems: "center", padding: "5rem 0rem", justifyContent: "center"}} >       
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d204.9238241043464!2d-58.49014997869312!3d-34.735909526147694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1695075959473!5m2!1ses-419!2sar" width="400" height="250" style={{position: "relative" ,  border:"0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   )
