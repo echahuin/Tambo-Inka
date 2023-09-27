2224e867143432622d22272efada540d4797ca7f
Edgar <edgachahuinauccafuro@frba.utn.edu.ar>
2023-09-26 21:07:06 -0300

    update galeria imgs

--- app/Home/Sections/Galeria/index.jsx
+++ app/Home/Sections/Galeria/index.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import classes from './styleGaleria.module.css'
+import Image from 'next/image'
+
+const data =[
+  {
+    img: "",
+    heightImg: [1,2],
+    widhtImg: [1,3]
+  },
+  {
+    img: "",
+    heightImg: [1,2],
+    widhtImg: [1,3]
+  },
+  {
+    img: "",
+    heightImg: [1,2],
+    widhtImg: [1,3]
+  },
+  {
+    img: "",
+    heightImg: [1,2],
+    widhtImg: [1,3]
+  },
+  {
+    img: "",
+    heightImg: [1,2],
+    widhtImg: [1,3]
+  }
+]
 
 function index() {
   return (
-    <div id="galeria">Galeria hi</div>
+    <div id="galeria" className={classes.contGaleria} >
+      <div className={classes.child_0} >
+        <Image style={{objectFit: "cover"}} src={"/images/food.jpg"} alt="" fill={true} />
+      </div>
+      <div className={classes.child_1}>
+        <Image style={{objectFit: "cover"}} src={"/images/original/plato.jpg"} alt="" fill={true} />
+      </div>
+      <div className={classes.child_2}>
+        <Image style={{objectFit: "cover"}} src={"/images/original/roger.jpg"} alt="" fill={true} />
+      </div>
+      <div className={classes.child_3}>
+        <Image style={{objectFit: "cover"}} src={"/images/asparagus.jpg"} alt="" fill={true} />
+      </div>
+      <div className={classes.child_4}>
+        <Image style={{objectFit: "cover"}} src={"/images/hamburger.jpg"} alt="" fill={true} />
+      </div>
+      <div className={classes.child_5}>
+        <Image style={{objectFit: "cover"}} src={"/images/original/vocaditos.jpg"} alt="" fill={true} />
+      </div>
+    </div>
   )
 }
 
--- app/Home/page.js
+++ app/Home/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             <PrincipalSlider />
             <Services />
             {/* <ServicesDetail /> */}
-            {/* <Galeria /> */}
+            <Galeria />
             <Map/>
             <Footer />
         </div>
