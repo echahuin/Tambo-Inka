0517844656357cd13d551fb33ca906f8bf63cf55
Edgar <edgachahuinauccafuro@frba.utn.edu.ar>
2023-10-22 16:50:10 -0300

    update galeria item mobile

--- app/Detail/[id]/page.js
+++ app/Detail/[id]/page.js
@@ -7,11 +7,15 @@ import classes from './styles.module.css';
 export default function OnliImage({params}) {
     const { id } = params;
     const objData = dataGaleria.find(item => item.id === parseInt(id));
-    console.log(objData);
 
     return(
-        <div>
-          <Image src={objData.img} fill={true} alt='' />
+        <div className={classes.bodyItem}>
+          <div style={{width: "100%", height:"7rem",  background: "#000000ac ", position: "absolute", position:"fixed", top:"0"}} ></div>
+            <Image className={classes.img} src={objData.img} height={600} width={600}   alt={`itemImg${id}`} />
+            <div className={classes.contText}>
+              <h1>{objData.title}</h1>
+              <p>{objData.description}</p>
+            </div>
         </div>
     )
 }
--- app/data/dataGaleria.js
+++ app/data/dataGaleria.js
@@ -3,31 +3,37 @@ const dataGaleria =[
     {
       img: "/images/food.jpg",
       id: 0,
+      title: "Plato de comida one",
       description: "0 lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
       img: "/images/original/plato.jpg",
       id: 1,
+      title: "Plato de comida one",
       description: "1 lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
       img: "/images/original/roger.jpg",
       id: 2,
+      title: "Plato de comida one",
       description: "2 lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
       img: "/images/asparagus.jpg",
       id: 3,
+      title: "Plato de comida one",
       description: "2 lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
       img: "/images/hamburger.jpg",
       id: 4,
+      title: "Plato de comida one",
       description: "4 lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
       img: "/images/original/vocaditos.jpg",
       id: 5,
+      title: "Plato de comida one",
       description: "5 lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     }
   ]
