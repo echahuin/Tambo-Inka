beb3a1b90d615942a1469d813b4a377b367db83f
Edgar <edgachahuinauccafuro@frba.utn.edu.ar>
2023-10-23 22:20:37 -0300

    add button whats app

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import Services from './Sections/Services'
 import Footer from './Sections/Footer'
 import Galeria from './Sections/Galeria'
 import Map from './Sections/Map'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
 
@@ -13,6 +15,12 @@ export default function Home() {
       <Galeria />
       <Map/>
       <Footer />
+      {/* <div> */}
+        <Link href={"https://api.whatsapp.com/send/?phone=5491125599997&text&type=phone_number&app_absent=0"} style={{position: "fixed", bottom: "0", right: "0", margin: "1rem"}}>
+          {/* click me */}
+          <Image alt="linkWhatsApp" src={"/images/social/whatsappLogo.png"} height={55} width={55} style={{boxShadow: "var(--cardShadowSmall)",  borderRadius: "100px",}}/>
+        </Link>
+      {/* </div> */}
     </div>
   )
 }
